23c404e2a0651de484d843b8d5d2404c6ff5e406
Lydia Ebinghaus <lydiakathrin.ebbinghaus@stud.tu-darmstadt.de>
2024-01-09 15:48:01 +0100

    refactored splitting helpers

--- pytorch-image-models/preparation/splitting_helpers.py
+++ pytorch-image-models/preparation/splitting_helpers.py
@@ -44,6 +44,29 @@ def add_missing_files_to_df(df, missing_files):
     print(len(df))
     df.to_csv("../images/full_dataset_mod.csv")
 
+
+def print_nested_dict(dictionary: dict, title: str):
+    for d in dictionary:
+        print("\n\n", title, ":", d)
+        print("{:<20} {:<15}".format('Label', 'Count'))
+        for k, item in dictionary[d].items():
+            print("{:<20} {:<15}".format(k, item))
+
+def save_video_dict():
+    classes = dataset["label"].unique()
+    video_dict = {}
+
+    for video, group_df in dataset.groupby("video", as_index=False):
+        current_dict = dict.fromkeys(classes, 0)
+        for label in classes:
+            if label in group_df["label"].unique():
+                count = group_df["label"].value_counts()[label]
+                current_dict[label] += count
+        video_dict[video] = current_dict
+
+    pd.DataFrame.from_dict(video_dict).to_csv("../images/video_dict.csv")
+    return video_dict
+
 class_dict = {
     "blood_hematin": "Blood",
     "ampulla_of_vater": "Ampulla",
@@ -57,21 +80,3 @@ if __name__ == '__main__' :
     # Add missing files if necessary
     # missing_files = get_missing_files(dataset)
     # add_missing_files_to_df(dataset, missing_files)
-    classes = dataset["label"].unique()
-    videos = dataset["video"].unique()
-    video_dict = {}
-
-    for video, group_df in dataset.groupby("video", as_index=False):
-        current_dict = dict.fromkeys(classes, 0)
-        for label in classes:
-            if label in group_df["label"].unique():
-                count = group_df["label"].value_counts()[label]
-                current_dict[label] += count
-        video_dict[video] = current_dict
-
-    pd.DataFrame.from_dict(video_dict).to_csv("../images/video_dict.csv")
-    for v in video_dict:
-        print("\n\nVIDEO:", v)
-        print("{:<20} {:<15}".format('Label', 'Count'))
-        for k, item in video_dict[v].items():
-            print("{:<20} {:<15}".format(k, item))
