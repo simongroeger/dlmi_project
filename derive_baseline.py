cf6c3ee34ef220e2bb8342c26d8306cafb13efa7
Lydia Ebinghaus <lydiakathrin.ebbinghaus@stud.tu-darmstadt.de>
2024-01-27 16:16:08 +0100

    support for using patch with the smallest hue for prediction (working semi good)

--- derive_baseline.py
+++ derive_baseline.py
@@ -6,18 +6,18 @@ import matplotlib.pyplot as plt
 from numpy.core.numeric import allclose
 from tqdm import trange
 
-def main():
 
-    src_dir = "/home/simon/dlmi_project/pytorch-image-models/images"
+def main():
+    src_dir = "./pytorch-image-models/images"
     split = "val_split"
 
     cls_folder_list = os.listdir(os.path.join(src_dir, split))
 
     classcount = np.zeros(len(cls_folder_list))
-    
+
     hs = []
     h_all = []
-    for cls_i, cls_folder in enumerate(cls_folder_list): 
+    for cls_i, cls_folder in enumerate(cls_folder_list):
         print(cls_folder)
         img_list = os.listdir(os.path.join(src_dir, split, cls_folder))
         h = []
@@ -25,11 +25,13 @@ def main():
             img_name = img_list[i]
             if "_rot_" in img_name or "_shear_" in img_name:
                 continue
-            img = cv2.imread(os.path.join(src_dir, split, cls_folder, img_name))/255.0
+            img = cv2.imread(os.path.join(src_dir, split, cls_folder, img_name)) / 255.0
             hsv_image = matplotlib.colors.rgb_to_hsv(img)
-            average_h = np.mean(hsv_image[:, :, 0])
-            h.append(average_h)
-            h_all.append(average_h)
+
+            # Use patches mean h for of the area with the smallest mean h (since 0-60 is redish)
+            smallest_mean_h = min(mean_h_for_patches(hsv_image, 3))
+            h.append(smallest_mean_h)
+            h_all.append(smallest_mean_h)
             classcount[cls_i] += 1
         hs.append(h)
 
@@ -40,7 +42,7 @@ def main():
     all_counts, bins = np.histogram(h_all, 100, density=True)
     print(bins)
 
-    for cls_i, cls_folder in enumerate(cls_folder_list): 
+    for cls_i, cls_folder in enumerate(cls_folder_list):
         likelihood, _ = np.histogram(hs[cls_i], bins, density=True)
         ax[0].stairs(likelihood, bins, fill=False, label="p(mean hue | " + cls_folder + ")")
 
@@ -49,8 +51,6 @@ def main():
 
         posterior = likelihood_prior / all_counts * 100
         ax[2].stairs(posterior, bins, fill=False, label="p(" + cls_folder + " | mean hue)")
-
-
 
     ax[0].set_title("Likelihood")
     ax[1].set_title("Likelihood x Prior")
@@ -65,6 +65,21 @@ def main():
     plt.show()
 
 
+"""
+Calculate and return mean h for all num_patches patches of the image
+"""
+
+
+def mean_h_for_patches(img, num_patches) -> list[float]:
+    patch_hs = []
+    patch_size = img.shape[0] // num_patches
+    for i in range(num_patches):
+        if i == num_patches - 1:
+            average_h = np.mean(img[i * patch_size:, i * patch_size:, 0])
+        else:
+            average_h = np.mean(img[i * patch_size:(i + 1) * patch_size, i * patch_size:(i + 1) * patch_size, 0])
+        patch_hs.append(average_h)
+    return patch_hs
 
 
 main()
